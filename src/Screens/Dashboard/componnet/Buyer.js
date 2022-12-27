830518b0d0868543042f4748d125d55404832207
vaishnaviReyna <vaishnavi.chauhan@reynasolutions.com>
2022-12-27 17:53:55 +0530

    changes

--- src/Screens/Dashboard/componnet/Buyer.js
+++ src/Screens/Dashboard/componnet/Buyer.js
@@ -22,26 +22,46 @@ import {
   spaceVertical,
 } from '../../../styles/variables';
 import {Picker} from '@react-native-picker/picker';
+import { ActivityIndicator } from 'react-native-paper';
 
 const Buyer = ({navigation}) => {
   const [productList, setProductList] = useState([]);
+  const [categoryFilter, setCategory] = useState("all");
+  const [clotheslist, setClothesList] = useState('');
+  const [grocerylist, setGroceryList] = useState('');
+  const [loading, setloading] = useState(true)
 
   useEffect(() => {
+    console.log("hello i am new");
     const dbRef = ref(db, 'categoryLists/');
-
     onValue(dbRef, snapshot => {
-      let records = [];
-      snapshot.forEach(childSnapshot => {
-        let keyName = childSnapshot.key;
-        let data = childSnapshot.val();
-        records.push({data});
-      });
-      const arra1 = records[0].data;
-      const arra2 = records[1].data;
-      setProductList(arra1.concat(arra2));
+      if(snapshot){
+        let records = [];
+        snapshot.forEach(childSnapshot => {
+          let data = childSnapshot.val();
+          records.push({data});
+          // setloading(false)
+        });
+        setClothesList(records[0].data);
+        setGroceryList(records[1].data);
+        setProductList([...records[0].data,...records[1].data])
+      }
     });
   }, []);
-  console.log('productList', productList);
+
+  useEffect(() => {
+    switch (categoryFilter) {
+      case 'grocery':
+        setProductList(grocerylist);
+        break;
+      case 'clothes':
+        setProductList(clotheslist);
+        break;
+      default:
+        setProductList([...grocerylist,...clotheslist])
+    }
+  }, [categoryFilter]);
+
   const renderItem = ({item}) => (
     <View style={styles.productlistview}>
       <Image style={styles.productimg} source={{uri: `${item.avatar}`}} />
@@ -52,8 +72,7 @@ const Buyer = ({navigation}) => {
             color: colors.HARD_BLACK,
             fontFamily: fontFamily.semiBold,
             width: responsiveWidth(50),
-          }}
-        >
+          }}>
           {item.details}
         </Text>
         <Text style={{color: colors.green, fontFamily: fontFamily.medium}}>
@@ -66,6 +85,7 @@ const Buyer = ({navigation}) => {
     </View>
   );
 
+  console.log("product", productList);
   return (
     <View style={styles.buyercontainer}>
       <StatusBar
@@ -76,70 +96,71 @@ const Buyer = ({navigation}) => {
       />
 
       <View style={styles.listview}>
-        <View style={{flexDirection: 'row', justifyContent:'space-between'}}>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
           <View style={styles.searchbarview}>
             <TextInput
               style={styles.searchinput}
               placeholder="Search Here"
-              placeholderTextColor={'gray'}
-            ></TextInput>
+              placeholderTextColor={'gray'}></TextInput>
 
             <TouchableOpacity>
               <Image
                 resizeMode="contain"
                 style={styles.searchicon}
-                source={require('../../../Assests/Images/search.png')}
-              ></Image>
+                source={require('../../../Assests/Images/search.png')}></Image>
             </TouchableOpacity>
           </View>
           <View
             style={{
               borderRadius: borderRadius.medium,
-              borderWidth: 1.5,borderColor:'#bbb',
-              alignSelf: 'center',justifyContent:'center',
-              height:responsiveHeight(6),top:9,marginLeft:marginHorizontal.semiSmall
-            }}
-          >
-           
+              borderWidth: 1.5,
+              borderColor: '#bbb',
+              alignSelf: 'center',
+              justifyContent: 'center',
+              height: responsiveHeight(6),
+              top: 9,
+              marginLeft: marginHorizontal.semiSmall,
+            }}>
             <Picker
               style={{
                 width: responsiveWidth(30),
                 borderRadius: borderRadius.medium,
-                borderWidth: 1,alignSelf:'center',justifyContent:'center',
+                borderWidth: 1,
+                alignSelf: 'center',
+                justifyContent: 'center',
                 borderColor: colors.grayline,
               }}
-              selectedValue={productList}
-              onValueChange={itemValue => setProductList(itemValue)}
-             
-            >
-              <Picker.Item label="Filter" value="category" />
-              <Picker.Item label="grocery" value="grocery" />
-              <Picker.Item label="clothes" value="clothes" />
-              <Picker.Item label="All" value="All" />
-
-        
+              selectedValue={categoryFilter}
+              onValueChange={itemValue => setCategory(itemValue)}>
+              <Picker.Item label="All" value="all" />
+              <Picker.Item label="Grocery" value="grocery" />
+              <Picker.Item label="Clothes" value="clothes" />
             </Picker>
           </View>
         </View>
-        {productList.length <= 0 ? (
-          <Text
-            style={{
-              fontFamily: fontFamily.bold,
-              marginTop: spaceVertical.normal,
-              fontSize: fontSize.large,
-            }}
-          >
-            No products available
-          </Text>
-        ) : (
-          <FlatList
-            data={productList}
-            renderItem={renderItem}
-            keyExtractor={item => item.id}
-            contentContainerStyle={{paddingBottom: spaceVertical.extraLarge}}
-            showsVerticalScrollIndicator={false}
-          />
-        )}
+
+        {!loading ? <ActivityIndicator animating={true} color="green"  style={{alignContent:"center",justifyContent:"center",height:responsiveHeight(80)}}/>:
+ <>
+ {productList.length == 0 ? (
+   <Text
+     style={{
+       fontFamily: fontFamily.bold,
+       marginTop: spaceVertical.normal,
+       fontSize: fontSize.large,
+     }}>
+     No products available
+   </Text>
+ ) : (
+   <FlatList
+     data={productList}
+     renderItem={renderItem}
+     keyExtractor={item => item.id}
+     contentContainerStyle={{paddingBottom: spaceVertical.extraLarge}}
+     showsVerticalScrollIndicator={false}
+   />
+ )}
+ </>
+        }
       </View>
     </View>
   );
